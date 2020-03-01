38e641b537109a18974ec8e7d51546e9e69e6335
Lupachik <lupachik@gmail.com>
2020-03-01 18:20:34 +0300

    HW04-2 - update method save for (updateNotFound)

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -28,7 +28,10 @@ public class JpaMealRepository implements MealRepository {
             em.persist(meal);
             return meal;
         } else {
-            return em.merge(meal);
+            Meal findMeal = em.find(Meal.class, meal.getId()); // for test updateNotFound
+            if (findMeal != null && findMeal.getUser().getId() == userId) {
+                return em.merge(meal);
+            } else return null;
         }
     }
 

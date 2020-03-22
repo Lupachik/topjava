65b908a7b6c8862006c106f06a15d8a71ffc028a
Lupachik <lupachik@gmail.com>
2020-03-22 03:42:49 +0300

    HW06(optional) - 2.3 - add realisation user roles in JdbcUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -9,10 +10,13 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 @Transactional(readOnly = true)
@@ -44,12 +48,17 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update(
-                "UPDATE users SET name=:name, email=:email, password=:password, " +
-                        "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
-            return null;
+            insertRole(user);
+        } else {
+            if (namedParameterJdbcTemplate.update(
+                    "UPDATE users SET name=:name, email=:email, password=:password, " +
+                            "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
+                return null;
+            }
+            deleteRole(user);
+            insertRole(user);
         }
-        return user;
+        return setRolesFromDb(user);
     }
 
     @Transactional
@@ -61,18 +70,65 @@ public class JdbcUserRepository implements UserRepository {
     @Override
     public User get(int id) {
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        return setRolesFromDb(DataAccessUtils.singleResult(users));
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        return setRolesFromDb(DataAccessUtils.singleResult(users));
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+//        http://bazhenov.me/blog/2014/07/20/java-8-top10.html
+//        https://www.tutorialspoint.com/java/util/enumset_noneof.htm
+        Map<Integer, Set<Role>> mapRoles = new HashMap<>(); // get map where key -  user_id, value set roles
+        jdbcTemplate.query("SELECT * FROM user_roles", rs -> {
+            mapRoles.computeIfAbsent(rs.getInt("user_id"), u -> EnumSet.noneOf(Role.class))
+                    .add(Role.valueOf(rs.getString("role")));
+        });
+
+        List<User> userList = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        userList.forEach(user -> user.setRoles(mapRoles.get(user.getId())));
+        return userList;
     }
+
+    // get list user roles from db
+    private User setRolesFromDb(User user) {
+        List<Role> roles = jdbcTemplate.query("SELECT role FROM user_roles WHERE user_id=?",
+                (rs, rowNum) -> Role.valueOf(rs.getString("role")), user.getId());
+        user.setRoles(roles);
+        return user;
+    }
+
+    // methods for insert/delete role
+
+    private void deleteRole(User user) {
+        jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
+    }
+
+
+    //    https://mkyong.com/spring/spring-jdbctemplate-batchupdate-example/
+    private void insertRole(User user) {
+        Set<Role> roleSet = user.getRoles();
+        List<Role> roleList = new ArrayList<>(roleSet);
+
+        if (roleSet.size() != 0) {
+            jdbcTemplate.batchUpdate("INSERT INTO user_roles (user_id, role) VALUE (?, ?)", new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    ps.setInt(1, user.getId());
+                    ps.setString(2, roleList.get(i).toString());
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return roleSet.size();
+                }
+            });
+        }
+    }
+
 }
